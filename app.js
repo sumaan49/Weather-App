8f9e89500ac6494c2facabffd3fd75b66bf837a3
sumaan49 <sumaan.karki174@gmail.com>
2021-02-19 03:00:02 -0800

    Implement functionality to toggle between fahrenheit and celcius

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+const refinedData = {};
 
-function getTime() {
-  const time = document.querySelector('.time');
-  let currentTime = new Date().toLocaleString();
-  time.innerHTML = currentTime;
-}
+const city = document.querySelector('.location');
+const status = document.querySelector('.status');
+const temperature = document.querySelector('.temperature');
+const max = document.querySelector('.max');
+const min = document.querySelector('.min');
+const feel = document.querySelector('.feel');
+const humidity = document.querySelector('.humidity');
+
+
 
 const userInput = document.querySelector('.userInput');
 const form = document.querySelector('form');
@@ -23,41 +28,91 @@ async function getWeather(location) {
     display(processedData);
 
   } catch(error) {
-    alert(error)
+    console.log(error)
   }
 }
 
 function processData(data) {
-  const refinedData = {
-    location: data.name,
-    country: data.sys.country,
-    status: data.weather[0].description,
-    temperature: data.main.temp - 273,
-    max: data.main.temp_max,
-    min: data.main.temp_min
+  const temp = kelvinToFahrenheit(data.main.temp);
+
+  refinedData.temperature = {
+      value: temp,
+      unit: 'fahrenheit'
   };
+  refinedData.location = data.name;
+  refinedData.country = data.sys.country;
+  refinedData.status = data.weather[0].description
+  refinedData.max = kelvinToFahrenheit(data.main.temp_max);
+  refinedData.min = kelvinToFahrenheit(data.main.temp_min);
+  refinedData.feel = kelvinToFahrenheit(data.main.feels_like);
+  refinedData.humidity = data.main.humidity;
   return refinedData;
 }
 
 function display(data) {
-  const location = document.querySelector('.location');
-  const status = document.querySelector('.status');
-  const temperature = document.querySelector('.temperature');
-
-  location.innerHTML = `${data.location}, ${data.country}`;
+  city.innerHTML = `${data.location}, ${data.country}`;
   status.innerHTML = data.status;
-  temperature.innerHTML = toFahrenheit(data.temperature);
+  humidity.innerHTML = data.humidity + '%';
+  temperature.innerHTML = data.temperature.value + '&degF';
+  max.innerHTML = data.max + '&degF';
+  min.innerHTML = data.min + '&degF';
+  feel.innerHTML = data.feel + '&degF';
+}
+const checkbox = document.querySelector("[name=checkbox]");
+checkbox.addEventListener('change', toggleTemperature);
 
+
+function toggleTemperature(e) {
+  if (!e.target.checked) {
+    temperature.innerHTML = toCelsius(refinedData.temperature.value) + '&degC';
+    max.innerHTML = toCelsius(refinedData.max) + '&degC';
+    min.innerHTML = toCelsius(refinedData.min) + '&degC';
+    feel.innerHTML = toCelsius(refinedData.feel) + '&degC';
+  } else {
+    temperature.innerHTML = refinedData.temperature.value + '&degF';
+    max.innerHTML = refinedData.max + '&degF';
+    min.innerHTML = refinedData.min + '&degF';
+    feel.innerHTML = refinedData.feel + '&degF';
+  };
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function toFahrenheit(temperature) {
-  temperature = parseFloat(temperature);
   temperatureerature = Math.round((temperature = (temperature - 32) * (5/9)));
   return temperature;
 }
 
 function toCelsius(temperature) {
-  temperature = parseFloat(temperature);
   temperature = Math.round((temperature = (temperature - 32) * (5 / 9)));
   return temperature;
 }
+
+function kelvinToFahrenheit(temperature) {
+  temperature = Math.round((1.8 * (temperature - 273)) + 32);
+  return temperature;
+}
+
+
+
+
+
